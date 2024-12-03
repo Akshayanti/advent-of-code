ca2d1cb5b55b9816f4c099472986bb227173f3e8
Akshay Aggarwal (aksaggar) <aksaggar@cisco.com>
2024-12-03 07:57:57 +0100

    D3 solution

---
+++ 2024/d03/d3.py
@@ -0,0 +1,35 @@
+import re
+
+input_file = "input.txt"
+# part1
+with open(input_file, "r") as f:
+    text = f.read()
+    pattern = r"mul\(\d{1,3},\d{1,3}\)"
+    matches = re.findall(pattern, text)
+    numbers = [re.findall(r'\d{1,3}', match) for match in matches]
+    part1 = 0
+    for nums in numbers:
+        part1 += int(nums[0]) * int(nums[1])
+    print(part1)
+
+
+# part2
+with open(input_file, "r") as f:
+    text = f.read()
+    pattern = r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"
+    matches = re.findall(pattern, text)
+    updated_matches = []
+    add_flag = True
+    for x in matches:
+        if x == "don't()":
+            add_flag = False
+        elif x == "do()":
+            add_flag = True
+        else:
+            if add_flag:
+                updated_matches.append(x)
+    mul_numbers = [re.findall(r'\d{1,3}', match) for match in updated_matches if match.startswith("mul")]
+    part2 = 0
+    for nums in mul_numbers:
+        part2 += int(nums[0]) * int(nums[1])
+    print(part2)
